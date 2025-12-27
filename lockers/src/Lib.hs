a1f8e0bc955443d134352d695dcfd137f929216b
Eitan Barylko <eitan@barylko.com>
2025-12-26 23:20:19 -0800

    Added type declarations

--- lockers/src/Lib.hs
+++ lockers/src/Lib.hs
@@ -1,6 +1,42 @@
 module Lib
-    ( someFunc
+    ( someFunc, createNLockers
     ) where
+
+import Data.Map
+
+newtype Positive = Positive { unPositive :: Int }
+
+toPositive :: Int -> Maybe Positive
+toPositive n = if (n <= 0) then Nothing else Just (Positive n)
+
+-- We can't export unPositive, because unPositive can be used
+-- to update the field.  Trivially renaming it to getPositive
+-- ensures that getPositive can only be used to access the field
+getPositive :: Positive -> Int
+getPositive = unPositive
+
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+type LockerId = Positive
+
+data LockerSize = Tiny | Small | Medium | Large | ExtraLarge
+
+data Locker = Locker {
+  id:: LockerId,
+  size:: LockerSize}
+
+data Lockers = Lockers {
+  idToLocker:: Map LockerId Locker,
+  lockerSizeToLockers:: Map LockerSize [Locker],
+  isValidLockerId:: LockerId -> Bool} deriving Show  
+
+{- Takes a positive number n and creates n lockers with the sizes of the
+lockers being either tiny, small, medium, large, or extra large
+-}
+createNLockers:: Positive -> Lockers
+
+createNLockers numOfLockers = Map.empty Map.empty (const True)
+
+
--- lockers/test/Spec.hs
+++ lockers/test/Spec.hs
@@ -1,4 +1,6 @@
 import Test.Hspec
 
+import Lib (createNLockers)
+
 main :: IO ()
 main = putStrLn "Test suite not yet implemented"
